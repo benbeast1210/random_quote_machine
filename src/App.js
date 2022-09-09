94526c005f03ae61f6aa6d234e7b465cfba59a96
Test User <testuser@Tests-MacBook-Pro.local>
2022-09-09 18:03:41 -0400

    merge-existing-files

--- src/App.js
+++ src/App.js
@@ -1,43 +1,39 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
-import QuoteBox from './QuoteBox';
 import Tweet from './Tweet';
 
 export default function App() {
 
   // RNG
-  const randomInt = Math.floor(Math.random() * 1643);
+  const randInt = Math.floor(Math.random() * 1643);
 
   // initialize state and assign it a random quote
   const [quotes, setQuotes] = useState([]);
-  const [quote, setQuote] = useState('');
+  const [quote, setQuote] = useState({});
 
   fetch("https://type.fit/api/quotes")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    setQuotes(data);
-  })
-  .catch((error) => {
-    //error.preventDefault();
-  });
-
-  // build refresh button to setText and setAuthor to random quotes
-  function refreshQuote() {
-    setQuote(quotes[randomInt]);
-  }
+    .then(function (response) { return response.json })
+    .then(function (data) { setQuotes(data) })
+  
+  //find a way to set the quote
 
   return (
     <div className="App">
-      <QuoteBox quote={quote} />
+      <div id='quote-box' style={{ alignItems: 'center' }}>
+
+        {/* (User Story #2 - exists√, #6 - appears on load) The text component will call from the API and display the quote text */}
+        <div id="text">{quote.text}</div> 
+
+        {/* (User Story #3 - exists√, #7 - appears on load) The author component will call from the API and display the quote author */}
+        <div id="author">- {quote.author}</div> 
+
+      </div>
 
       {/* (User Story #4 - exists√, #8 & #9 - functionality) This button will call the API and refresh the quote's Text and Author */}
-      <button id='new-quote' onClick={refreshQuote()}></button>
+      <button id='new-quote' onClick={useEffect(() => setQuote(quotes[randInt]), [quotes, randInt])}>New Quote</button>
       
       {/* (User Story #5 - exists√, #10 - functionality/path) This button is to tweet the quote */}
       <Tweet />
-
     </div>
   );
 }
--- src/Tweet.js
+++ src/Tweet.js
@@ -1,7 +1,7 @@
 export default function Tweet() {
   return (
     <button>
-      <a href='twitter.com/intent/tweet' id='tweet-quote' title="Tweet this quote!" target="_top"></a>
+      <a href="https://twitter.com/" id="tweet-quote" title="Tweet this quote!" target="_top">Tweet this quote!</a>
     </button>
   ); 
 }
