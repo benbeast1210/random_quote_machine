3ad92149d23b037796a6ab421b0c69b0c6baf6bb
Test User <testuser@Tests-MacBook-Pro.local>
2022-09-09 15:24:33 -0400

    merged-with-existing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import QuoteBox from './QuoteBox';
+import Tweet from './Tweet';
 
-function App() {
+export default function App() {
+
+  // RNG
+  const randomInt = Math.floor(Math.random() * 1643);
+
+  // initialize state and assign it a random quote
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState('');
+
+  fetch("https://type.fit/api/quotes")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    setQuotes(data);
+  })
+  .catch((error) => {
+    //error.preventDefault();
+  });
+
+  // build refresh button to setText and setAuthor to random quotes
+  function refreshQuote() {
+    setQuote(quotes[randomInt]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QuoteBox quote={quote} />
+
+      {/* (User Story #4 - exists√, #8 & #9 - functionality) This button will call the API and refresh the quote's Text and Author */}
+      <button id='new-quote' onClick={refreshQuote()}></button>
+      
+      {/* (User Story #5 - exists√, #10 - functionality/path) This button is to tweet the quote */}
+      <Tweet />
+
     </div>
   );
 }
-
-export default App;
