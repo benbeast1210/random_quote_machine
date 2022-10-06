cf046d21c7e3b6852c0d74ee6f64d5897b70d451
Test User <testuser@Tests-MacBook-Pro.local>
2022-10-06 18:05:39 -0400

    reviewing implementation, fixing funcitonality

--- src/App.js
+++ src/App.js
@@ -3,33 +3,37 @@ import './App.css';
 
 export default function App() {
 
+  
+  const BASE_URL = "https://type.fit/api/quotes";
+
+  // RNG
+  const randInt = Math.floor(Math.random() * quotes.length);
+
   // initialize state and assign it a random quote
   const [quotes, setQuotes] = useState([]);
   const [quote, setQuote] = useState({});
 
-  fetch("https://type.fit/api/quotes")
-    .then(function (response) { return response.json })
-    .then(function (data) { setQuotes(data) })
-  
-  //find a way to set the quote onLoad()
+  // Effect
+  useEffect(() => setQuote(quotes[randInt]), [quotes, randInt])
 
-  // RNG
-  const randInt = Math.floor(Math.random() * quotes.length);
+  // fetch and process response as json
+  
+  // find a way to set the quote onLoad()
 
   return (
     <div className="App">
       <div id='quote-box' style={{ alignItems: 'center' }}>
 
         {/* (User Story #2 - exists√, #6 - appears on load) The text component will call from the API and display the quote text */}
-        <div id="text">{quote.text}</div> 
+        <div id="text">{quote.text}</div>
 
         {/* (User Story #3 - exists√, #7 - appears on load) The author component will call from the API and display the quote author */}
-        <div id="author">- {quote.author}</div> 
+        <div id="author">- {quote.author}</div>
 
       </div>
 
       {/* (User Story #4 - exists√, #8 & #9 - functionality) This button will call the API and refresh the quote's Text and Author */}
-      <button id='new-quote' onClick={useEffect(() => setQuote(quotes[randInt]), [quotes, randInt])}>New Quote</button>
+      <button id='new-quote' onClick={}>New Quote</button>
       
       {/* (User Story #5 - exists√, #10 - functionality/path) This button is to tweet the quote */}
       <button>
